640e2ef9146ddecefa31a5e7a56e59ada8682885
CaptainGlac1er <CaptainGlac1er@GWCGAMING>
2016-11-27 15:30:08 -0500

    Add project files.

---
+++ FunMath/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace FunMath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.largestNum = new System.Windows.Forms.TextBox();
+            this.numOutput = new System.Windows.Forms.TextBox();
+            this.prevPageBtn = new System.Windows.Forms.Button();
+            this.nextPageBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numPerPage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gotoPageTb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(596, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(596, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // largestNum
+            // 
+            this.largestNum.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.largestNum.Location = new System.Drawing.Point(146, 439);
+            this.largestNum.Name = "largestNum";
+            this.largestNum.Size = new System.Drawing.Size(462, 20);
+            this.largestNum.TabIndex = 2;
+            this.largestNum.Text = "10000";
+            // 
+            // numOutput
+            // 
+            this.numOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numOutput.Location = new System.Drawing.Point(13, 38);
+            this.numOutput.Multiline = true;
+            this.numOutput.Name = "numOutput";
+            this.numOutput.ReadOnly = true;
+            this.numOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.numOutput.Size = new System.Drawing.Size(595, 366);
+            this.numOutput.TabIndex = 3;
+            // 
+            // prevPageBtn
+            // 
+            this.prevPageBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.prevPageBtn.Location = new System.Drawing.Point(13, 410);
+            this.prevPageBtn.Name = "prevPageBtn";
+            this.prevPageBtn.Size = new System.Drawing.Size(75, 23);
+            this.prevPageBtn.TabIndex = 4;
+            this.prevPageBtn.Text = "Prev Page";
+            this.prevPageBtn.UseVisualStyleBackColor = true;
+            this.prevPageBtn.Click += new System.EventHandler(this.prevPageBtn_Click);
+            // 
+            // nextPageBtn
+            // 
+            this.nextPageBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextPageBtn.Location = new System.Drawing.Point(533, 410);
+            this.nextPageBtn.Name = "nextPageBtn";
+            this.nextPageBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextPageBtn.TabIndex = 5;
+            this.nextPageBtn.Text = "NextPage";
+            this.nextPageBtn.UseVisualStyleBackColor = true;
+            this.nextPageBtn.Click += new System.EventHandler(this.nextPageBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 442);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Largest Number to Check:";
+            // 
+            // numPerPage
+            // 
+            this.numPerPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numPerPage.Location = new System.Drawing.Point(197, 412);
+            this.numPerPage.Name = "numPerPage";
+            this.numPerPage.Size = new System.Drawing.Size(208, 20);
+            this.numPerPage.TabIndex = 7;
+            this.numPerPage.Text = "1000";
+            this.numPerPage.TextChanged += new System.EventHandler(this.numPerPage_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Numbers per page:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(411, 415);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Goto Page:";
+            // 
+            // gotoPageTb
+            // 
+            this.gotoPageTb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.gotoPageTb.Location = new System.Drawing.Point(473, 412);
+            this.gotoPageTb.Name = "gotoPageTb";
+            this.gotoPageTb.Size = new System.Drawing.Size(53, 20);
+            this.gotoPageTb.TabIndex = 10;
+            this.gotoPageTb.Text = "1";
+            this.gotoPageTb.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 496);
+            this.Controls.Add(this.gotoPageTb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numPerPage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nextPageBtn);
+            this.Controls.Add(this.prevPageBtn);
+            this.Controls.Add(this.numOutput);
+            this.Controls.Add(this.largestNum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "PrimeFinder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox largestNum;
+        private System.Windows.Forms.TextBox numOutput;
+        private System.Windows.Forms.Button prevPageBtn;
+        private System.Windows.Forms.Button nextPageBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numPerPage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox gotoPageTb;
+    }
+}
+
---
+++ FunMath/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunMath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            array = new bool[Int32.Parse(largestNum.Text)];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = false;
+            Stopwatch sw = Stopwatch.StartNew();
+            calcPrimes();
+            sw.Stop();
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == false)
+                    count++;
+            Stopwatch sw2 = Stopwatch.StartNew();
+            display();
+            sw2.Stop();
+            curCount = count;
+            textBox1.Text = "Primes found: " + count + " in " + sw.ElapsedMilliseconds + "ms Rendered in " + sw2.ElapsedMilliseconds  + "ms\r\n";
+        }
+        private void display()
+        {
+            StringBuilder output = new StringBuilder();
+            int pageNum = Int32.Parse(numPerPage.Text);
+            output.Append(((page - 1) * pageNum) + " through " + ((page) * pageNum) + " numbers \r\n\r\n");
+            for (int i = (page - 1) * pageNum; i < page * pageNum && i < array.Length; i++)
+            {
+                if(!array[i])
+                    output.Append( (i + 1) + "\r\n");
+            }
+            gotoPageTb.Text = page + "";
+            numOutput.Text = output.ToString();
+
+        }
+        private int page = 1;
+        private int curCount = 0;
+        private bool[] array;
+        private void calcPrimesRec(Int64 start)
+        {
+            if (start >= array.Length)
+                return;
+            for(Int64 i = start * 2 - 1; i < array.Length; i += start)
+            {
+                if (array[i])
+                    continue;
+                array[i] = true;
+                calcPrimesRec(i);
+            }
+            if(!array[start])
+                calcPrimesRec(start + 1);
+        }
+        private void calcPrimes()
+        {
+            for (Int64 i = 2; i < array.Length; i++)
+                if (!array[i - 1])
+                    for (Int64 j = i * 2 - 1; j < array.Length; j += i)
+                        array[j] = true;
+        }
+
+        private void nextPageBtn_Click(object sender, EventArgs e)
+        {
+            int pageNum = Int32.Parse(numPerPage.Text);
+            if (array.Length > (page) * pageNum)
+            {
+                page++;
+                display();
+            }
+            gotoPageTb.Text = page + "";
+        }
+
+        private void prevPageBtn_Click(object sender, EventArgs e)
+        {
+            if(page > 1)
+                page--;
+            display();
+            gotoPageTb.Text = page + "";
+        }
+
+        private void numPerPage_TextChanged(object sender, EventArgs e)
+        {
+            display();
+            page = 1;
+            gotoPageTb.Text = page + "";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            page = Int32.Parse(gotoPageTb.Text);
+            Int32 pageNum = Int32.Parse(numPerPage.Text);
+            if (array.Length < page * pageNum)
+                page = array.Length / pageNum;
+            display();
+        }
+       
+    }
+}
